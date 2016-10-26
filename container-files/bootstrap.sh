53457ce8fa7aee3b5c18d94b4975121096e1096b
Tommi Palomäki <tommi.palomaki@digia.com>
2016-10-26 16:42:43 +0300

    We cannot modify the mapped config file in place

--- container-files/bootstrap.sh
+++ container-files/bootstrap.sh
@@ -2,10 +2,17 @@
 set -eu
 export TERM=xterm
 
-# Default Agent ConfigFile if not set by docker
+# Use the default config file if not set by an environment variable
 if [ -z "$CONFIG_FILE" ]; then
-  CONFIG_FILE="/usr/local/etc/zabbix_agentd.conf"
+  SRC_CONFIG_FILE="/usr/local/etc/zabbix_agentd.conf"
+else
+  SRC_CONFIG_FILE="$CONFIG_FILE"
 fi
+
+# This is the actual config file used, containing changes implied by environment variables
+TARGET_CONFIG_FILE=$(tempfile)
+log "Loading config: ${SRC_CONFIG_FILE}"
+cp "$SRC_CONFIG_FILE" "$TARGET_CONFIG_FILE"
 
 # Bash Colors
 red=`tput setaf 1`
@@ -19,25 +26,26 @@ separator=$(echo && printf '=%.0s' {1..100} && echo)
 log() {
   if [[ "$@" ]]; then echo "${bold}${green}[LOG `date +'%T'`]${reset} $@"; else echo; fi
 }
-### Magic Starts here
+
+### Update config file based on environment variables
 update_config() {
     log "Updating configuration file..."
-    if [[ "$ZABBIX_SERVER" != "127.0.0.1" ]]; then
+    if [ ! -z "$ZABBIX_SERVER" ] && [[ "$ZABBIX_SERVER" != "127.0.0.1" ]]; then
       log "Changing Zabbix Server IP to ${bold}${white}${ZABBIX_SERVER}${reset}"
-      sed -i 's/Server=127.0.0.1/Server='$ZABBIX_SERVER'/g' ${CONFIG_FILE}
+      sed -i 's/Server=127.0.0.1/Server='$ZABBIX_SERVER'/g' ${TARGET_CONFIG_FILE}
     fi
-    if [[ "$HOSTNAME" != "zabbix.agent" ]]; then
+    if [ ! -z "$HOSTNAME" ] && [[ "$HOSTNAME" != "zabbix.agent" ]]; then
       log "Changing Zabbix Hostname to ${bold}${white}${HOSTNAME}${reset}."
-      sed -i 's/Hostname=Zabbix server/Hostname='$HOSTNAME'/g' ${CONFIG_FILE}
+      sed -i 's/Hostname=Zabbix server/Hostname='$HOSTNAME'/g' ${TARGET_CONFIG_FILE}
     fi
-    if [[ "$HOST_METADATA" != "zabbix.agent" ]]; then
+    if [ ! -z "$HOST_METADATA" ] && [[ "$HOST_METADATA" != "zabbix.agent" ]]; then
       log "Changing Zabbix Host Metadata to ${bold}${white}${HOST_METADATA}${reset}."
-      sed -i 's/# HostMetadata=/HostMetadata='$HOST_METADATA'/g' ${CONFIG_FILE}
+      sed -i 's/# HostMetadata=/HostMetadata='$HOST_METADATA'/g' ${TARGET_CONFIG_FILE}
     fi
     log "Config updated"
 }
 print_config() {
-  log "Current Zabbix Agent config ${CONFIG_FILE}:"
+  log "Current Zabbix Agent config:"
   printf '=%.0s' {1..100} && echo
   cat ${CONFIG_FILE}
   echo ""
@@ -49,25 +57,8 @@ start() {
     zabbix_agentd -f -c ${CONFIG_FILE}
 }
 
-if [[ $(grep "million12/zabbix-agent" ${CONFIG_FILE}) ]]; then
-    log "Loading default config."
-    update_config
-    start
-else
-    log "Loading custom config: ${CONFIG_FILE}"
-    start
-fi
-
-
-
-
-
-
-
-
-
-
-
+update_config
+start
 
 
 
